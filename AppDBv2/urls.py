d16f98db8289763ab383110601bfb8de7e446343
Kalter <dmitry@gnuhost.eu>
2018-02-13 15:45:48 +0100

    static fix + docker files

--- AppDBv2/urls.py
+++ AppDBv2/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
 from AppDBv2 import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^$', views.page_1, name="page 1"),
     url(r'^page_2/', views.page_2, name="page 2"),
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
