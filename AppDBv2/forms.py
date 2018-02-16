b019baaff32148f528262c55053c6b2494b4df65
Kalter <dmitry@gnuhost.eu>
2018-02-16 17:00:48 +0100

    added login form

--- AppDBv2/forms.py
+++ AppDBv2/forms.py
@@ -1,5 +1,11 @@
 from django import forms
 from models import *
+from django.utils.translation import ugettext as _
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(label=_(u'Username'), max_length=30)
+    password = forms.CharField(label=_(u'Password'), widget=forms.PasswordInput)
 
 
 class DataTableForm (forms.Form):
--- AppDBv2/urls.py
+++ AppDBv2/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
 from AppDBv2 import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    url(r'^$', views.page_1, name="page 1"),
+    url(r'^$', login, {'template_name': 'login.html'}),
+    url(r'^page_1/', views.page_1, name="page 1"),
     url(r'^page_2/', views.page_2, name="page 2"),
     url(r'^admin/', admin.site.urls),
 ]
