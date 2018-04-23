4a4b32bc8434c82d1f62a9fddede999e594233b5
Kalter <dmitry@gnuhost.eu>
2018-04-23 16:53:39 +0200

    settings update

--- AppDBv2/settings.py
+++ AppDBv2/settings.py
@@ -107,4 +107,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR+'/AppDBv2/', 'static'), )
+
 LOGIN_REDIRECT_URL = 'page 1'
