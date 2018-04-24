db536319bf19e6e3e16a58f58867013f428eb48d
Kalter <dmitry@gnuhost.eu>
2018-04-24 09:50:20 +0200

    Settings fix

--- AppDBv2/settings.py
+++ AppDBv2/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'AppDBv2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR + '/AppDB/templates/'],
+        'DIRS': [BASE_DIR + '/AppDBv2/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,8 +104,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR+'/AppDBv2/', 'static'), )
 
--- AppDBv2/urls.py
+++ AppDBv2/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 from AppDBv2 import views
-from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from django.conf import settings
 from views import custom_login
 
 urlpatterns = [
@@ -13,5 +11,3 @@ urlpatterns = [
     url(r'^page_2$', views.page_2, name="page 2"),
     url(r'^admin/', admin.site.urls),
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
