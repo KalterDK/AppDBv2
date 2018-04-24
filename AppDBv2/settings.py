706b1b9a4461bf4cc259103d13467ba189aade14
Kalter <dmitry@gnuhost.eu>
2018-04-24 13:22:38 +0200

    Added lables on pages

--- AppDBv2/settings.py
+++ AppDBv2/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'AppDBv2.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 DATABASE_ROUTERS = ('AppDBv2.dbrouters.DBRouter',)
 
-DATABASES
+DATABASES = DATABASES
 
 
 # Password validation
